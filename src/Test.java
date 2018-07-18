4826f616126eee76a35dd45b70b88e3d71a0fbd9
Wu <FWU31@ford.com>
2018-07-18 17:50:20 +0800

    change to scanner

--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
         File f = new File("j.txt");
         try {
             FileInputStream fip = new FileInputStream(f);
-            InputStreamReader reader = new InputStreamReader(fip);
 
+            Scanner sc = new Scanner(fip);
             StringBuffer sb = new StringBuffer();
 
-            while(reader.ready()) {
-                char currentChar = (char)reader.read();
-                sb.append(currentChar);
-                if(currentChar == '\n' || currentChar == '\r') {
-                    sb.append("haha\n");
-                }
+//            while(reader.ready()) {
+//                char currentChar = (char)reader.read();
+//                sb.append(currentChar);
+//                if(currentChar == '\n' || currentChar == '\r') {
+//                    sb.append("haha\n");
+//                }
+//            }
+
+            //reader.read(char[] cbuf);
+            List<String> words = new LinkedList<>();
+            while (sc.hasNext()){
+                words.add(sc.next());
             }
 
+            ListIterator<String> wordsList = words.listIterator();
+
+            while(wordsList.hasNext()) {
+                String current = wordsList.next();
+                //String next = wordsList.next();
+                //wordsList.previous();
+                sb.append(current + "\n");
+                //if(!current.equals("haha")&& !next.equals("haha")) {
+                    sb.append("haha\n");
+                //}
+            }
             System.out.println(sb);
-            reader.close();
+            //reader.close();
             fip.close();
 
             FileOutputStream fop = new FileOutputStream(f);
